c41cc72747501e0c634256e83d1e909faf6cc400
wangcd <cd.wang@cargosmart.com>
2016-10-06 17:30:52 +0800

    init page

--- VueDjango/FreeMap/app/main/main.js
+++ VueDjango/FreeMap/app/main/main.js
@@ -1,4 +1,8 @@
+require('./main.css');
 var Vue = require("vue");
+var VueStrap = require('vue-strap');
+var L = require('leaflet');
+
 
 Vue.config.delimiters = ['${', '}']
 
@@ -6,5 +10,14 @@ var app = new Vue({
     el: '#app',
     data:{
         message:'this is page'
+    },
+    map: null,
+    ready:function(){
+        var osm =L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        });
+        this.map = L.map('main-map',{
+            layers: [osm]
+        }).setView(new L.LatLng(0,0), true);
     }
 })
--- VueDjango/webpack.config.js
+++ VueDjango/webpack.config.js
@@ -3,16 +3,31 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry: {
-      main:'./FreeMap/app/main/main.js'
-  },
-  output: {
-      path: path.resolve('./static/bundles/'),
-      filename: "[name]-[hash].js"
-  },
-
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'})
-  ]
+    context: __dirname,
+    entry: {
+        main:'./FreeMap/app/main/main.js'
+    },
+    output: {
+        path: path.resolve('./static/bundles/'),
+        filename: "[name]-[hash].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel"
+            },
+            {
+                test:/\.css$/,
+                loader: "style!css"
+            },
+            {
+                test:/\.(png)|(jpg)$/,
+                loader: "url?limit=50000"
+            }
+        ]
+    },
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'})
+    ]
 }
